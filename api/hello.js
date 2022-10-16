97086536ae22c2f40f927d7081b46a1310590fdd
AbudiMutamba <mutambaabudi@gmail.com>
2022-10-17 01:13:32 +0300

    Serveless function test

--- api/hello.js
+++ api/hello.js
@@ -1,40 +1,40 @@
 require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_ROLE_KEY
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseKey = process.env.REACT_APP_SUPABASE_ROLE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-export default function async ( req, res ) {
-    // try {
-    //     const { email, password } = req.body
+export default async function ( req, res ) {
+    try {
+        const { email, password } = req.body
 
-    //     if ( !email ) {
-    //         const response = { "Status":"Failure","Details": "Email address not provided"}
-    //         return res.status(400).json(response)
-    //     }
+        if ( !email ) {
+            const response = { "Status":"Failure","Details": "Email address not provided"}
+            return res.status(400).json(response)
+        }
 
-    //     if ( !password ) {
-    //         const response = { "Status": "Failure", "Details": "Password not provided"}
-    //         return res.status(400).json(response)
-    //     }
+        if ( !password ) {
+            const response = { "Status": "Failure", "Details": "Password not provided"}
+            return res.status(400).json(response)
+        }
 
-    //     const { data: user, error } = await supabase.auth.api.createUser({
-    //         email,
-    //         password,
-    //         password_confirm: true
-    //     })
+        const { data: user, error } = await supabase.auth.api.createUser({
+            email,
+            password,
+            password_confirm: true
+        })
 
-    //     if ( error ) throw error
-    //     if ( user ) {
-    //        const response = { "Status": "Success", "Details": "The user has been created"}
-    //        return res.status(400).json(response)
-    //     }
+        if ( error ) throw error
+        if ( user ) {
+           const response = { "Status": "Success", "Details": "The user has been created"}
+           return res.status(400).json(response)
+        }
         
-    // } catch ( error ){
-    //     const response = {"Status":"Failure", "Details":error}
-    //     return res.status(400).json(response)      
-    // }
+    } catch ( error ){
+        const response = {"Status":"Failure", "Details":error}
+        return res.status(400).json(response)      
+    }
     res.statusCode = 200;
     res.json({ message: 'It works' });
 }
--- api/world.js
+++ api/world.js
@@ -9,36 +9,7 @@ const supabase = createClient(supabaseUrl, supabaseKey)
 
 
 export default async function (req, res)  {
-  // res.statusCode = 200;
-  // res.json({ message: 'It works' });
-  try {
-        const { email, password } = req.body
-
-        if ( !email ) {
-            const response = { "Status":"Failure","Details": "Email address not provided"}
-            return res.status(400).json(response)
-        }
-
-        if ( !password ) {
-            const response = { "Status": "Failure", "Details": "Password not provided"}
-            return res.status(400).json(response)
-        }
-
-        const { data: user, error } = await supabase.auth.api.createUser({
-            email,
-            password,
-            password_confirm: true
-        })
-
-        if ( error ) throw error
-        if ( user ) {
-           const response = { "Status": "Success", "Details": "The user has been created"}
-           return res.status(400).json(response)
-        }
-        
-    } catch ( error ){
-        const response = {"Status":"Failure", "Details":error}
-        return res.status(400).json(response)      
-    }
-
+  res.statusCode = 200;
+  res.json({ message: 'It works' });
+ 
 }
